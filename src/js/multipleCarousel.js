f1f1afbd388cbff5c1e7c08067f2186c9eaa77c9
AlexTrubkina <sashatru99@gmail.com>
2022-12-10 17:37:30 +0300

    changed carousel news

--- src/js/multipleCarousel.js
+++ src/js/multipleCarousel.js
@@ -1,30 +1,38 @@
 const container = document.querySelector("#carouselNews");
-let carouselItems = container.querySelectorAll(".carousel-inner .carousel-item");
 
-const button = document.querySelector(".greenButton");
-// if (i !== carouselItems.length - 1) {
-    //     nextSibling = carouselItems[i+1];
-    //     clone = nextSibling.cloneNode(true);
-    //     item.append(clone);
-    //     console.log(item, i+1);
-    // }
-    // else {
-    //     prevSibling = carouselItems[i-1];
-    //     clone = prevSibling.cloneNode(true);
-    //     item.append(clone)
-    //     console.log(item, i-1)
-    // }
+if (window.matchMedia("(min-width: 992px)").matches) {
+    let carousel = new bootstrap.Carousel(container, {
+        interval: false,
+    });
+    const carouselInner = container.querySelector(".carousel-inner");
+    const carouselWidth = carouselInner.scrollWidth;
+    const card = container.querySelector(".carousel-item");
+    const cardWidth = card.scrollWidth;
 
-carouselItems.forEach((item, i) => {
-    length = 3;
-    next = item.nextElementSibling;
-    for (i = 1; i < length; i++) {
-        if (!next) {
-            next = carouselItems[0];
+    let scrollPosition = 0;
+
+    const carouselControlNext = container.querySelector(".carousel-control-next");
+    const carouselControlPrev = container.querySelector(".carousel-control-prev");
+
+    carouselControlNext.onclick = () => {
+        console.log(carouselWidth, cardWidth, scrollPosition);
+        if (scrollPosition < (carouselWidth - cardWidth * 3)) {
+            scrollPosition += cardWidth;
+            carouselInner.animate({
+                scrollLeft: scrollPosition
+            }, 600);
         }
-        let cloneChild = next.cloneNode(true);
-        item.appendChild(cloneChild.children[0]);
-        next = next.nextElementSibling;
-        console.log(item);
-    }
-});
+    };
+
+    carouselControlPrev.onclick = () => {
+        if (scrollPosition > 0) {
+            scrollPosition -= cardWidth;
+            carouselInner.animate({
+                scrollLeft: scrollPosition
+            }, 600);
+        }
+    };
+}
+else {
+    container.className += " slide";
+}
