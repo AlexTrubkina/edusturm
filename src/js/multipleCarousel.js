474a1a7204e63caf0aabd13e199675505a220942
Александра Трубкина <aleksandratrubkina@MacBook-Air-Aleksandra.local>
2022-12-11 21:57:10 +0300

    changed fonts h1 and fixed carouel

--- src/js/multipleCarousel.js
+++ src/js/multipleCarousel.js
@@ -1,38 +1,31 @@
-const container = document.querySelector("#carouselNews");
-
-if (window.matchMedia("(min-width: 992px)").matches) {
-    let carousel = new bootstrap.Carousel(container, {
-        interval: false,
-    });
-    const carouselInner = container.querySelector(".carousel-inner");
-    const carouselWidth = carouselInner.scrollWidth;
-    const card = container.querySelector(".carousel-item");
-    const cardWidth = card.scrollWidth;
-
-    let scrollPosition = 0;
-
-    const carouselControlNext = container.querySelector(".carousel-control-next");
-    const carouselControlPrev = container.querySelector(".carousel-control-prev");
-
-    carouselControlNext.onclick = () => {
-        console.log(carouselWidth, cardWidth, scrollPosition);
-        if (scrollPosition < (carouselWidth - cardWidth * 3)) {
-            scrollPosition += cardWidth;
-            carouselInner.animate({
-                scrollLeft: scrollPosition
-            }, 600);
-        }
-    };
-
-    carouselControlPrev.onclick = () => {
-        if (scrollPosition > 0) {
-            scrollPosition -= cardWidth;
-            carouselInner.animate({
-                scrollLeft: scrollPosition
-            }, 600);
-        }
-    };
-}
-else {
-    container.className += " slide";
+const multipleCardCarousel = document.querySelector(
+  "#carouselNews"
+);
+if (window.matchMedia("(min-width: 768px)").matches) {
+  var carousel = new bootstrap.Carousel(multipleCardCarousel, {
+    interval: false,
+  });
+  const carouselWidth = $(".carousel-inner")[0].scrollWidth;
+  const cardWidth = $(".carousel-item").width();
+  let scrollPosition = 0;
+  $("#carouselNews .carousel-control-next").on("click", function () {
+    if (scrollPosition < carouselWidth - cardWidth * 4) {
+      scrollPosition += cardWidth;
+      $("#carouselNews .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+  $("#carouselNews .carousel-control-prev").on("click", function () {
+    if (scrollPosition > 0) {
+      scrollPosition -= cardWidth;
+      $("#carouselNews .carousel-inner").animate(
+        { scrollLeft: scrollPosition },
+        600
+      );
+    }
+  });
+} else {
+  $(multipleCardCarousel).addClass("slide");
 }
