2f33e8891db18ad6a9dbab7ce1312e7a5115e883
AlexTrubkina <sashatru99@gmail.com>
2022-12-16 14:00:44 +0300

    scroll auto

--- src/js/chat.js
+++ src/js/chat.js
@@ -5,7 +5,7 @@
             cardBody = document.querySelector(".card-body");
             if (this.value !== "") {
                 newMessage = `<div class="chat-user justify-content-end align-items-center w-100 mb-3">
-                                    <div class="chat-user-message" >` + this.value + `</div><img src="../images/avatar.png" alt="Avatar"></div>`
+                                    <div class="chat-user-message" >` + this.value + `</div><img src="../../../images/avatar.png" alt="Avatar"></div>`
                 cardBody.innerHTML += newMessage;
                 this.value = "";
             }
