3bd93579d8849527eb61ed29a3aa453fb8f22c06
AlexTrubkina <sashatru99@gmail.com>
2023-01-05 14:02:20 +0300

    key code to key, scroll to bottom

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,21 +1,42 @@
 // отправка сообщения после нажатия на кнопку
-
-(function() {
-    const cards = document.querySelectorAll('.card');
+let cardBody = document.querySelector(".card-body");
+(function () {
+    const cards = document.querySelectorAll(".card");
+    const anchor = document.querySelector(".anchor");
     // если на странице больше одного чата
     cards.forEach((item, i) => {
-        const input = item.querySelector('input');
-        input.addEventListener('keydown', function(e) {
-            let newMessage = '';
-            if (e.keyCode === 13) {
+        const input = item.querySelector("input");
+        input.addEventListener("keydown", function (e) {
+            if (e.key === 'Enter') {
                 const cardBody = item.querySelector(".card-body");
                 if (this.value !== "") {
-                    newMessage = `<div class="chat-user justify-content-end align-items-center w-100 mb-3">
-                                        <div class="chat-user-message" >` + this.value + `</div><img src="../../../images/avatar.png" alt="Avatar"></div>`
-                    cardBody.innerHTML += newMessage;
+                    const parentBlock = document.createElement("div");
+                    parentBlock.className =
+                        "chat-user justify-content-end align-items-center w-100 mb-3";
+                    const userMessage = document.createElement("div");
+                    userMessage.className = "chat-user-message";
+                    const text = document.createTextNode(this.value);
+                    userMessage.appendChild(text);
+                    const img = document.createElement("img");
+                    img.setAttribute("src", "../../../images/avatar.png");
+                    img.setAttribute("alt", "avatar");
+                    parentBlock.appendChild(userMessage);
+                    parentBlock.appendChild(img);
+                    // console.log(parentBlock);
+                    cardBody.insertBefore(parentBlock, anchor);
+                    // newMessage = `<div class="chat-user justify-content-end align-items-center w-100 mb-3">
+                    //                     <div class="chat-user-message" >` + this.value + `</div><img src="../../../images/avatar.png" alt="Avatar"></div>`
+                    // cardBody.innerHTML += newMessage;
                     this.value = "";
+                    cardBody.scrollTo(0, cardBody.scrollHeight);
                 }
             }
         });
     });
 })();
+
+
+            
+   
+
+
